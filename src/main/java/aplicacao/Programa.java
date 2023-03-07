7320df8ae1bf5a0ff1f5fdce68d5f42d3cc237f8
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-07 00:27:34 -0300

    buscando no DB com JPA

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -9,22 +9,14 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		
-		//Id nulo porque vai ser incrementado automaticamente.
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		
-		
+			
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
+		//Busca dados no DB 
+		Pessoa p = em.find(Pessoa.class, 2);
 		
+		System.out.println(p);
 		System.out.println("Pronto!");
 		
 		em.close();
